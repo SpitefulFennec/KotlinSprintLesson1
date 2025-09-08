3153693272752422aa81bf78ee3158cc8fa2f730
SFennec <spitefulfennec@gmail.com>
2025-09-09 01:32:54 +0200

    KSL-1-2 Initiate more variables

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson1
+
+fun main() {
+    var sumOfOrders: Int = 75
+    println("sum of orders = $sumOfOrders")
+    var thankingOrderRespond: String = "Спасибо за покупку! Ждём вас снова!"
+    println("thanking respond = $thankingOrderRespond")
+//    var sumOfEmployees: Int = 2000
+//    println("sum of employees = $sumOfEmployees")
+    var sumOfEmployees: Int = 1999
+    println("sum of employees = $sumOfEmployees")
+}
