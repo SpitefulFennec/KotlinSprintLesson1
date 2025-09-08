3b967c2736208cdd04c8d3375108b5b733f743bb
SFennec <spitefulfennec@gmail.com>
2025-09-09 01:53:58 +0200

    KSL-1-2 Initiate more variables

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println("0$hour")
+    println("0$minute")
+
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    print(minute)
+
+    /* BTW, в условиях задачи не сказано, что во второй раз нужно инициировать переменные по отдельности,
+поэтому можно сделать и через println(hour + ":" + minute) и через println("$hour:$minute")
+*/
+}
