0c110b988c022a0ed1a4cda021823a8205c2dc22
SFennec <spitefulfennec@gmail.com>
2025-09-09 01:17:57 +0200

    KSL-1-1 Initiate variables

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -2,5 +2,5 @@ package org.example.lesson1
 
 fun main() {
     var sumOfOrders: Int = 75
-    var thankingOrderRespond: String = "Спасибо за покупку!"
+    var thankingOrderRespond: String = "Спасибо за покупку! Ждём вас снова!"
 }
