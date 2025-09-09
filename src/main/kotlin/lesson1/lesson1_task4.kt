1bd7774d65599d8253fdcc0e5f9e593059321aff
SFennec <spitefulfennec@gmail.com>
2025-09-09 02:25:37 +0200

    KSL-1-4 Different type variables

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    println(length)
+    val age: Byte = 27
+    println(age)
+    val partOfDay: Float = 0.075f
+    println(partOfDay)
+    val seconds: Short = 6480
+    println(seconds)
+    val partOfYear: Double = 2.0547945205479453E-4
+    println(partOfYear)
+    val apogee: Int = 327000
+    println(apogee)
+}
